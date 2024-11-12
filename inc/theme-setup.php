87aaf3af5de388588deab828b5b4497bd905133d
Vhornets <vhornets@gmail.com>
2024-11-12 16:43:20 +0200

    add customize-selective-refresh-widgets theme support

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -26,6 +26,7 @@ function vh_after_setup_theme()
     add_theme_support('automatic-feed-links');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_theme_support('customize-selective-refresh-widgets');
     add_theme_support('responsive-embeds');
     add_theme_support(
         'html5',
